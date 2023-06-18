9c4f5b234dac01a780b972eb2cfb4b0291e246a9
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-06-18 16:47:54 +0100

    feat: constellations system

--- src/buttons/registrar.js
+++ src/buttons/registrar.js
@@ -3,26 +3,55 @@ module.exports = async (client, interaction) => {
 
 	interaction.reply({ content: "Feito!" });
 
-	const guild = await client.db.Guilds.findOne({ _id: argumentos[2] });
-	if (guild) {
-		guild.staffs.push({ _id: argumentos[1], function: argumentos[3] });
-		const member = interaction.guild.members.cache.get(argumentos[1]);
-		member.roles.add("1106772028280815707");
-		if (guild.roleId && guild.roleId !== "") {
-			member.roles.add(guild.roleId);
-			guild.save();
+	const guildDoc = await client.db.Guilds.findOne({ _id: argumentos[2] });
+	if (guildDoc) {
+		guildDoc.staffsmember.roles.add({
+			_id: argumentos[1],
+			function: argumentos[3],
+		});
+		const guildMember = interaction.guild.members.cache.get(argumentos[1]);
+		guildMember.roles.add("1106772028280815707");
+		const guildCache = client.guilds.cache.get(argumentos[2]);
+		if (!guildCache)
+			return interaction.channel.send({
+				content:
+					"Constelação não definida pois não tenho esse servidor na minha cache.",
+			});
+		if (guildCache.memberCount > 10000)
+			guildMember.roles.add("1106772076574019664");
+		if (guildCache.memberCount > 8000 && guildCache.memberCount <= 10000)
+			guildMember.roles.add("1120015301065584661");
+		if (guildCache.memberCount > 6000 && guildCache.memberCount <= 8000)
+			guildMember.roles.add("1106772078046228501");
+		if (guildCache.memberCount > 4000 && guildCache.memberCount <= 6000)
+			guildMember.roles.add("1106772077492576266");
+		if (guildCache.memberCount > 2000 && guildCache.memberCount <= 4000)
+			guildMember.roles.add("1120015317003943946");
+		if (guildCache.memberCount > 1000 && guildCache.memberCount <= 2000)
+			guildMember.roles.add("1120015320694927360");
+		if (guildCache.memberCount > 500 && guildCache.memberCount <= 1000)
+			guildMember.roles.add("1120015323790331945");
+		if (guildCache.memberCount > 100 && guildCache.memberCount <= 500)
+			guildMember.roles.add("1120015324528521277");
+		else
+			return interaction.channel.send({
+				content:
+					"Este servidor não cumpre os requisitos mínimos da sua constelação...",
+			});
+		if (guildDoc.roleId && guildDoc.roleId !== "") {
+			guildMember.roles.add(guildDoc.roleId);
+			guildDoc.save();
 		} else {
 			const server = client.guilds.cache.get(argumentos[2]);
 			interaction.guild.roles
 				.create({
 					name: server ? server.name : "Sakurai não detectou?",
-					color: "#5d83b3",
 					reason: "Novo cargo para registro do utilizador",
 				})
 				.then((role) => {
-					member.roles.add(role);
-					guild.roleId = role.id;
-					guild.save();
+					guildMember.roles.add(role);
+					guildDoc.roleId = role.id;
+					guildDoc.save();
 				});
 		}
 	} else {
