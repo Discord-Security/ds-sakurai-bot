b246f779f918943a6f8ff5ce3f696be97774285b
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-06-18 17:06:12 +0100

    fixes: constellation roles system

--- src/buttons/registrar.js
+++ src/buttons/registrar.js
@@ -16,33 +16,50 @@ module.exports = async (client, interaction) => {
 			});
 		if (guildCache.memberCount > 10000)
 			guildMember.roles.add("1106772076574019664");
-		if (guildCache.memberCount > 8000 && guildCache.memberCount <= 10000)
+		else if (
+			guildCache.memberCount > 8000 &&
+			guildCache.memberCount <= 10000
+		)
 			guildMember.roles.add("1120015301065584661");
-		if (guildCache.memberCount > 6000 && guildCache.memberCount <= 8000)
+		else if (
+			guildCache.memberCount > 6000 &&
+			guildCache.memberCount <= 8000
+		)
 			guildMember.roles.add("1106772078046228501");
-		if (guildCache.memberCount > 4000 && guildCache.memberCount <= 6000)
+		else if (
+			guildCache.memberCount > 4000 &&
+			guildCache.memberCount <= 6000
+		)
 			guildMember.roles.add("1106772077492576266");
-		if (guildCache.memberCount > 2000 && guildCache.memberCount <= 4000)
+		else if (
+			guildCache.memberCount > 2000 &&
+			guildCache.memberCount <= 4000
+		)
 			guildMember.roles.add("1120015317003943946");
-		if (guildCache.memberCount > 1000 && guildCache.memberCount <= 2000)
+		else if (
+			guildCache.memberCount > 1000 &&
+			guildCache.memberCount <= 2000
+		)
 			guildMember.roles.add("1120015320694927360");
-		if (guildCache.memberCount > 500 && guildCache.memberCount <= 1000)
+		else if (guildCache.memberCount > 500 && guildCache.memberCount <= 1000)
 			guildMember.roles.add("1120015323790331945");
-		if (guildCache.memberCount > 100 && guildCache.memberCount <= 500)
+		else if (guildCache.memberCount > 100 && guildCache.memberCount <= 500)
 			guildMember.roles.add("1120015324528521277");
 		else
-			return interaction.channel.send({
+			interaction.channel.send({
 				content:
 					"Este servidor não cumpre os requisitos mínimos da sua constelação...",
 			});
+
 		if (guildDoc.roleId && guildDoc.roleId !== "") {
 			guildMember.roles.add(guildDoc.roleId);
 			guildDoc.save();
 		} else {
-			const server = client.guilds.cache.get(argumentos[2]);
 			interaction.guild.roles
 				.create({
-					name: server ? server.name : "Sakurai não detectou?",
+					name: guildCache
+						? guildCache.name
+						: "Sakurai não detectou?",
 					reason: "Novo cargo para registro do utilizador",
 				})
 				.then((role) => {
