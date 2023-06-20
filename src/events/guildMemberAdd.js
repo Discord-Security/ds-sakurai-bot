f8be9fcbe4ba9883ff0abe15ba97f0edfae96273
Lyke ᵉᵗʳ <78686667+Lyke-etr@users.noreply.github.com>
2023-06-20 13:24:19 -0300

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -4,8 +4,8 @@ moment.locale("pt-br");
 module.exports = async (client, member) => {
 	if (member.user.bot) return;
 
-	if (member.guild.id === "936656115524042823") {
-		client.channels.cache.get("936678638324170762").send({
+	if (member.guild.id === "1106771396757037076") {
+		client.channels.cache.get("1106772099529453568").send({
 			content: `<a:Welcome:946180052893171722> **Seja bem vindo(a) ao ${member.guild.name} ${member.user}, leia nossas <#1106772093846175794> se apresente em <#1007356127983452300>,caso esteja interessado(a) em recrutar staffs pro seu servidor use o <#1107605928984973353>, caso precise do suporte da aliança crie um ticket em <#1106772102264127558>**`,
 		});
 		const superagent = require("superagent");
@@ -20,7 +20,7 @@ module.exports = async (client, member) => {
 				const callbackRes =
 					callback._body.generated_text.match(/"([^"]*)"/g)[0];
 				if (callbackRes.length > 0)
-					client.channels.cache.get("936678638324170762").send({
+					client.channels.cache.get("1106772099529453568").send({
 						content: callbackRes
 							.replace("&lt;", "<")
 							.replace("&gt;", ">")
